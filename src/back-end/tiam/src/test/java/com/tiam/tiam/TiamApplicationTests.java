58fa1bbab655fb01751fce91d862ddf8e65b014a
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2025-10-16 20:17:46 -0300

    cria projeto back-end

---
+++ src/back-end/tiam/src/test/java/com/tiam/tiam/TiamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiam.tiam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
