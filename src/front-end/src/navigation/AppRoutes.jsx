17a445b37d00f04d13cd11b481a8defa75c4959b
Samuel Maia <153786965+samuelmaia1@users.noreply.github.com>
2025-10-26 22:38:42 -0300

    feat: corrige autenticacao e cria rota de login

--- src/front-end/src/navigation/AppRoutes.jsx
+++ src/front-end/src/navigation/AppRoutes.jsx
@@ -1,14 +1,37 @@
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { Home } from '../screens/Home/Home';
 import { SignIn } from '../screens/SignIn/SignIn';
+import { useAuth } from '../hooks/useAuth';
+import { Dashboard } from '../screens/Dashboard/Dashboard';
+import { ActivityIndicator, View } from 'react-native';
+import { Login } from '../screens/Login/Login';
+import { useTheme } from 'styled-components';
 
 const Stack = createNativeStackNavigator()
 
 export default function AppRoutes() {
+  const {user, loading} = useAuth()
+  const theme = useTheme()
+
+    if (loading) {
+      return (
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <ActivityIndicator size="large" color={theme.colors.primary}/>
+        </View>
+      );
+    }
+
   return (
-    <Stack.Navigator screenOptions={{ headerShown: false }}>
-      <Stack.Screen name="Home" component={Home} />
-      <Stack.Screen name="Cadastro" component={SignIn} />
+    <Stack.Navigator screenOptions={{ headerShown: false }} initialRouteName={user ? 'Dashboard' : 'Home'}>
+       {user ? (
+          <Stack.Screen name="Dashboard" component={Dashboard} />
+        ) : (
+          <>
+            <Stack.Screen name="Home" component={Home} />
+            <Stack.Screen name="Cadastro" component={SignIn} />
+            <Stack.Screen name="Login" component={Login} />
+          </>
+        )}
     </Stack.Navigator>
   );
 }
