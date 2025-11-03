54f11e7fa7e402bf65abe935b160bf3674a486df
Bruna Borges <bruna.lborges.matias@gmail.com>
2025-11-02 21:21:37 -0300

    ajustando a rota

--- src/front-end/src/screens/Financial_Transactions/Financial_Transactions.jsx
+++ src/front-end/src/screens/Financial_Transactions/Financial_Transactions.jsx
@@ -1,11 +1,10 @@
 import { Container } from "../../styles/global";
+import { Text } from "react-native";
 
 export function Financial_Transactions(){
     return(
         <Container>
-            <View>
-                <Text>Movimentações</Text>
-            </View>
+            <Text>Página de movimentações financeiras</Text>
         </Container>
     )
 }
