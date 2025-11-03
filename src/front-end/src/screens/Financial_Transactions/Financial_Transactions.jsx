6998b12596cd3534e1a5ae7fbfd205e4e2132f64
Bruna Borges <bruna.lborges.matias@gmail.com>
2025-11-02 21:04:09 -0300

    criando branch e pasta para tela de movimentacoes financeiras

---
+++ src/front-end/src/screens/Financial_Transactions/Financial_Transactions.jsx
@@ -0,0 +1,11 @@
+import { Container } from "../../styles/global";
+
+export function Financial_Transactions(){
+    return(
+        <Container>
+            <View>
+                <Text>Movimentações</Text>
+            </View>
+        </Container>
+    )
+}
