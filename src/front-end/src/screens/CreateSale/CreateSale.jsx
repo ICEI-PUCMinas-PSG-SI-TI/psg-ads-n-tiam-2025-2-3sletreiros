50a1d9663f8909336c640dd08ee3b41e8a426ff1
samuelmaia1 <smaia190500@gmail.com>
2025-12-02 16:24:48 -0300

    feat: adiciona seleção de itens para vendas

--- src/front-end/src/screens/CreateSale/CreateSale.jsx
+++ src/front-end/src/screens/CreateSale/CreateSale.jsx
@@ -1,16 +1,85 @@
+import { Button } from "@components/Button/Button";
 import { CustomAnimation } from "@components/CustomAnimation/CustomAnimation";
+import { EmptyList } from "@components/EmptyList/EmptyList";
+import { ProductItem } from "@components/ProductItem/ProductItem";
 import { Text } from "@components/Text/Text";
-import { View } from "react-native";
+import { useProducts } from "@hooks/useProducts";
+import { useEffect, useState } from "react";
+import { useColorScheme, View } from "react-native";
+import { FlatList } from "react-native-gesture-handler";
 import { Container } from "src/styles/global";
+import { ProductSelectionModal } from "./ProductSelectionModal/ProductSelectionModal";
+import { formatToBRL } from "@utils/formatter";
+import { useTheme } from "styled-components";
 
 export function CreateSale() {
-    
+    const colorScheme = useColorScheme()
+    const theme = useTheme()
+
+    const [addedProducts, setAddedProducts] = useState([])
+
+    const [selectingProduct, setSelectingProduct] = useState(false)
+
+    const [total, setTotal] = useState(0)
+
+    function handleConfirmProducts(selectedProducts) {
+        setAddedProducts(prev => {
+            const updated = [...prev]
+
+            selectedProducts.forEach(product => {
+                const exists = updated.find(p => p.id === product.id)
+
+                if (exists) {
+                    exists.quantity += 1
+                } else {
+                    updated.push({ ...product, quantity: product.quantity ?? 1 })
+                }
+            })
+
+            return updated
+        })
+    }
+
+
+    useEffect(() => {
+        console.log(addedProducts)
+        setTotal(() => {
+            return addedProducts.reduce((acc, item) => acc + item.price * item.quantity, 0);
+        })
+    }, [addedProducts])
+
     return (
         <Container>
+            <ProductSelectionModal
+                isVisible={selectingProduct}
+                onClose={() => setSelectingProduct(false)}
+                onConfirm={handleConfirmProducts}
+            />
             <View style={{alignItems: 'center', gap: 10, justifyContent: 'center'}}>
                 <CustomAnimation name={'sale'} size={120}/>
                 <Text>Lance novas vendas</Text>
             </View>
+            
+            <Button
+                flex
+                buttonStyle={colorScheme === 'dark' ? 'primary' : 'surface'}
+                style={{marginTop: 10}}
+                onPress={() => setSelectingProduct(true)}
+            >
+                Adicionar Produto
+            </Button>
+
+            <Text color={theme.colors.text.secondary} style={{marginTop: 15}}>Total do pedido: {formatToBRL(total)}</Text>
+
+            <FlatList
+                data={addedProducts}
+                showsVerticalScrollIndicator={false}
+                keyExtractor={(item, index) => index.toString()}
+                renderItem={({ item }) => (
+                    <ProductItem item={item}/>
+                )}
+                ListEmptyComponent={<EmptyList message={'Nenhum produto adicionado ainda'} />}
+            />
         </Container>
     )
 }
---
+++ src/front-end/src/screens/CreateSale/ProductSelectionModal/ProductSelectionModal.jsx
@@ -0,0 +1,84 @@
+import { FlatList, Image, Modal, View } from "react-native";
+import { Card, CenterContainer, ModalContent, Overlay, ProductInfoContainer } from "./style";
+import { useProducts } from "@hooks/useProducts";
+import { Text } from "@components/Text/Text";
+import { formatToBRL } from "@utils/formatter";
+import { useState } from "react";
+import { Button } from "@components/Button/Button";
+
+export function ProductSelectionModal({isVisible, onClose, onConfirm}) {
+    const {products} = useProducts()
+
+    const [selectedProducts, setSelectedProducts] = useState([])
+
+    function toggleSelect(product) {
+        setSelectedProducts(prev => {
+            const exists = prev.find(p => p.id === product.id)
+            if (exists) {
+                return prev.filter(p => p.id !== product.id)
+            }
+            return [...prev, { ...product, quantity: 1 }]
+        })
+    }
+
+    function handleConfirm() {
+        onConfirm(selectedProducts)
+        setSelectedProducts([]) 
+        onClose()
+    }
+
+    return (
+        <Modal
+            visible={isVisible}
+            onRequestClose={onClose}
+            transparent
+        >
+            <Overlay onPress={onClose}>
+                <CenterContainer>
+                    <ModalContent>
+                        <Text align={'center'} style={{marginBottom: 20}}>Selecione os produtos do pedido</Text>
+                        <FlatList
+                            data={products}
+                            showsVerticalScrollIndicator={false}
+                            keyExtractor={(item, index) => index.toString()}
+                            renderItem={({ item }) => (
+                                <ProductItem 
+                                    item={item}
+                                    selected={!!selectedProducts.find(p => p.id === item.id)}
+                                    onPress={() => toggleSelect(item)}
+                                />
+                            )}
+                        />
+
+                         <Button
+                            style={{ marginTop: 20 }}
+                            disabled={selectedProducts.length === 0}
+                            onPress={handleConfirm}
+                            buttonStyle={'success'}
+                            flex
+                        >
+                            Confirmar Seleção
+                        </Button>
+                    </ModalContent>
+                </CenterContainer>
+            </Overlay>
+        </Modal>
+    )
+}
+
+function ProductItem({ item, selected, onPress }) {
+    return (
+        <Card
+            selected={selected}
+            onTouchEnd={onPress}
+        >
+            <Image source={{uri: item.image}} style={{ width: 90, height: 90, borderRadius: 8, resizeMode: 'contain' }}/>
+            <ProductInfoContainer>
+                <Text style={{ marginBottom: 8}}>{item.name}</Text>
+                <Text color={'#b6b6b6ff'} numLines={1} style={{fontSize: 12}}>{(item.description)}</Text>
+                <Text color={'#b6b6b6ff'} style={{fontSize: 10}}>{formatToBRL(item.price)}</Text>
+                <Text color={'#b6b6b6ff'} style={{fontSize: 10}}>Estoque: {item.stock}</Text>
+            </ProductInfoContainer>
+        </Card>
+    )
+}
