301d24224c39d342fd0847112c6a3764d65ec296
samuelmaia1 <smaia190500@gmail.com>
2025-12-02 09:10:30 -0300

    ajuste design do item de produto

--- src/front-end/src/components/ProductItem/ProductItem.jsx
+++ src/front-end/src/components/ProductItem/ProductItem.jsx
@@ -1,6 +1,6 @@
 import { Text } from "@components/Text/Text";
 import { Card } from "@components/ProductItem/style";
-import { Image, Pressable } from "react-native";
+import { Image, View } from "react-native";
 import {formatToBRL} from "@utils/formatter"
 import { useNavigation } from "@react-navigation/native";
 
@@ -13,11 +13,13 @@ export function ProductItem({item}){
 
     return(
             <Card onPress={openDetails}>
-                <Image source={{uri: item.image}} style={{ width: '100%', height: 150, borderRadius: 8 }}/>
+                <View style={{ width: '100%', height: 150, borderRadius: 8, overflow: 'hidden'}}> 
+                    <Image source={{uri: item.image}} style={{ width: '100%', height: 150, borderRadius: 8, resizeMode: 'cover' }}/>
+                </View>
                 <Text variant="subtitle" style={{ marginTop: 10}}>{item.name}</Text>
-                <Text color={'#b6b6b6ff'}>{(item.description)}</Text>
-                <Text color={'#b6b6b6ff'}>{formatToBRL(item.price)}</Text>
-                <Text color={'#b6b6b6ff'}>Estoque: {item.stock}</Text>
+                <Text color={'#b6b6b6ff'} numLines={1} style={{fontSize: 12}}>{(item.description)}</Text>
+                <Text color={'#b6b6b6ff'} style={{fontSize: 10}}>{formatToBRL(item.price)}</Text>
+                <Text color={'#b6b6b6ff'} style={{fontSize: 10}}>Estoque: {item.stock}</Text>
             </Card>
 
     )
