7bc7087faf85ab032db57d20f509b4f725181da1
Bruna Borges <bruna.lborges.matias@gmail.com>
2025-12-05 20:57:48 -0300

    tela de detalhes do produto finalizada

---
+++ src/front-end/src/screens/ProductDetails/style.js
@@ -0,0 +1,12 @@
+import styled from "styled-components/native";
+
+export const DetailsContainer = styled.View`
+    border: 1px solid;
+    
+    border-radius: 10px;
+
+    margin-bottom: 15px;
+    padding: 15px;
+
+    max-height: 500px;
+`
